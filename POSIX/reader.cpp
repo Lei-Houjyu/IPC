d80511902e762e9d55bf6479542d5d5953186c2f
Haoyu Li <haoyu.li@sjtu.edu.cn>
2020-04-29 20:53:25 +0800

    IPC by shared memory and semaphore

---
+++ POSIX/reader.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include <semaphore.h>
+#include <sys/wait.h>
+
+#define NUM 3
+#define NAME "/shmem"
+#define SIZE (NUM * sizeof(int))
+
+int main() {
+  sem_t* sem_id = sem_open("/sem", O_CREAT, 0600, 0);
+  if (sem_id < SEM_FAILED) {
+    perror("Reader: sem_open failed!\n");
+  }
+
+  int fd = shm_open("/shmem", O_CREAT | O_RDWR, 0666);
+  if (fd < 0) {
+    perror("Reader: shm_open failed!\n");
+    return EXIT_FAILURE;
+  }
+
+  int *data =
+      (int *)mmap(0, SIZE, PROT_READ, MAP_SHARED, fd, 0);
+
+  if (sem_wait(sem_id) < 0) {
+    perror("Reader: sem_wait failed!\n");
+  }
+
+  for (int i = 0; i < NUM; ++i) {
+    printf("%d\n", data[i]);
+  }
+
+  if (sem_post(sem_id) < 0) {
+    perror("Reader: sem_post failed!\n");
+  }
+
+  munmap(data, SIZE);
+
+  close(fd);
+
+  shm_unlink(NAME);
+
+  return EXIT_SUCCESS;
+}
---
+++ POSIX/writer.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <sys/wait.h>
+
+#include <thread>
+#include <chrono>
+
+#define NUM 3
+#define SIZE (NUM * sizeof(int))
+
+int main() {
+  sem_t* sem_id = sem_open("/sem", O_CREAT, 0600, 0);
+  if (sem_id == SEM_FAILED) {
+    perror("Writer: sem_open failed!\n");
+  }
+
+  int fd = shm_open("/shmem", O_CREAT | O_RDWR, 0600);
+  if (fd < 0) {
+    perror("Writer: shm_open failed!\n");
+    return EXIT_FAILURE;
+  }
+
+  ftruncate(fd, SIZE);
+
+  int *data =
+      (int *)mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+  for (int i = 0; i < NUM; ++i) {
+    data[i] = i;
+  }
+
+  if (sem_post(sem_id) < 0) {
+    perror("Writer: sem_post failed!\n");
+  }
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  if (sem_wait(sem_id) < 0) {
+    perror("Writer: sem_wait failed!\n");
+  }
+
+  printf("Writer: finishing...\n");
+
+  if (sem_close(sem_id) != 0) {
+    perror("Writer: sem_close failed!\n");
+  }
+
+  if (sem_unlink("/sem") < 0) {
+    perror("Writer: sem_unlink failed!\n");
+  }
+
+  munmap(data, SIZE);
+
+  close(fd);
+
+  return EXIT_SUCCESS;
+}
